de75e64e0260dbd92cf2963b3fbae8a82c4be7d3
Trevoro <trevdrobbins@gmail.com>
2026-05-05 17:04:20 -0700

    Add missing files

---
+++ src/Orgeng/Platform/Windows/WindowsWindow.cpp
@@ -0,0 +1,73 @@
+#include "ogpch.h"
+#include "Platform/Windows/WindowsWindow.h"
+
+#include "Events/ApplicationEvent.h"
+#include "Events/MouseEvent.h"
+#include "Events/KeyEvent.h"
+#include "Log.h"
+
+namespace Orgeng {
+
+	static bool s_GLFWInitialized = false;
+
+	Window* Window::Create(const WindowProps& props) {
+		return new WindowsWindow(props);
+	}
+
+	WindowsWindow::WindowsWindow(const WindowProps& props)
+	{
+		Init(props);
+	}
+
+	WindowsWindow::~WindowsWindow()
+	{
+		Shutdown();
+	}
+
+	void WindowsWindow::Init(const WindowProps& props)
+	{
+		m_Data.Title = props.Title;
+		m_Data.Width = props.Width;
+		m_Data.Height = props.Height;
+
+		OG_INFO("Creating window {0} ({1}, {2})", props.Title, props.Width, props.Height);
+
+		if (!s_GLFWInitialized)
+		{
+			int success = glfwInit();
+			OG_ERROR("could not initialized GLFW");
+		}
+
+		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		glfwMakeContextCurrent(m_Window);
+		glfwSetWindowUserPointer(m_Window, &m_Data);
+		SetVSync(true);
+	}
+
+	void WindowsWindow::Shutdown()
+	{
+		glfwDestroyWindow(m_Window);
+	}
+
+	void WindowsWindow::OnUpdate()
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(m_Window);
+	}
+
+	void WindowsWindow::SetVSync(bool enabled)
+	{
+		if (enabled)
+			glfwSwapInterval(1);
+		else
+			glfwSwapInterval(0);
+
+		m_Data.VSync = enabled;
+	}
+
+	bool WindowsWindow::IsVSync() const
+	{
+		return m_Data.VSync;
+	}
+
+}
---
+++ src/Orgeng/ogpch.cpp
@@ -0,0 +1,2 @@
+#include "ogpch.h"
+
